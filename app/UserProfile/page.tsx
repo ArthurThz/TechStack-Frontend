8a9b9cba20443bd2014c584e267662b207d47a81
ArthurThz <arthur.theodoro.sva@hotmail.com>
2024-01-06 00:01:39 -0300

    updating user profile component

--- app/UserProfile/page.tsx
+++ app/UserProfile/page.tsx
@@ -3,33 +3,21 @@
 import React, { useState } from 'react'
 import Input from '../components/Layout/Input/Input'
 import Button from '../components/Layout/Button/Button'
+ 
 
 const UserProfile = () => {
   const [hideOptions,setHideOptions] = useState("hidden")
   const [hideEdit,setHideEdit] = useState("block")
   const [inputStatus,setInputStatus] = useState(true)
-  const userInfo  = {
-    nome:"aa",
-    sobrenome:"sss",
-    cpf:"fff",
-    telefone:"cccc",
-    email:"bbbb",
-    profissao:"nnnn"
  
-  } 
 
-  let userData = Object.keys(userInfo)
 
-  const data = ["Arthur","Theodoro",12345678910,"(11) 98888-8888","arthur@mail.com","Front-end Developer"]
-  console.log(console.log(typeof userInfo.cpf))
- 
-  userData.forEach((item : string) => {
-    // for(let i = 0; i < data.length; i++){
-      
-    // }
-  })
+  const data = ["Arthur","Theodoro","12345678910","(11) 98888-8888","arthur@mail.com","Front-end Developer"]
 
-  
+  const inputField = ["nome","sobrenome","cpf","telefone","email","profissao"]
+
+
+
 
   const showOptions = () =>{
 
@@ -69,24 +57,25 @@ const UserProfile = () => {
         <Button label='Cancelar' variant='secondary' classname={hideOptions} onclick={showOptions}/>
 
         </div>
-        <div className="w-full h-auto flex flex-row items-center gap-7 p-6">
 
+        
+        <div className="w-full h-auto grid grid-cols-2 grid-rows-3 gap-7 p-6">
 
-        <Input label='Nome' name='username' type='text' placeholder='Arthur'disabled={inputStatus} />
-        <Input label='Sobrenome' name='userlastname' type='text' placeholder='Theodoro' disabled={inputStatus} />
-        </div>
-        <div className="w-full h-auto flex flex-row items-center gap-7 p-6">
-
-
+        {data.map((userdata,index) => (
+          
+          <Input key={index} label='Nome' name='username' type='text' placeholder={userdata} disabled={inputStatus} />
+          
+        ))}
+          
+        
+        {/* <Input label='Sobrenome' name='userlastname' type='text' placeholder='Theodoro' disabled={inputStatus} />
         <Input label='CPF' name='cpf' type='text' placeholder='12345678910' disabled={inputStatus} />
         <Input label='Telefone' name='phone' type='text' placeholder='(11) 98888-8888' disabled={inputStatus} />
-        </div>
-        <div className="w-full h-auto flex flex-row items-center gap-7 p-6">
-
-
         <Input label='Email' name='mail' type='text' placeholder='arthur@mail.com' disabled={inputStatus} />
-        <Input label='Profissão' name='role' type='text' placeholder='Front-end Developer' disabled={inputStatus} />
+        <Input label='Profissão' name='role' type='text' placeholder='Front-end Developer' disabled={inputStatus} /> */}
         </div>
+
+       
     </div>
   )
 }
--- app/components/Layout/Input/Input.tsx
+++ app/components/Layout/Input/Input.tsx
@@ -7,7 +7,7 @@ const Input = ({label, name, onChange,type,disabled,placeholder}:IInput) => {
         <div className=" w-full flex flex-col h-auto ">
         <label className='text-green-haze-300 font-bold text-lg'
         htmlFor={name}>{label}</label>
-        <input className='min-w-[70%] border border-woodsmoke-950 outline-non disabled:bg-transparent disabled:border-green-haze-500 bg-woodsmoke-700 text-white h-12 rounded-md px-2 focus-within:outline-green-haze-500 '
+        <input className='min-w-[70%] border border-woodsmoke-950 outline-none disabled:bg-transparent disabled:border-green-haze-500 bg-woodsmoke-700 text-white h-12 rounded-md px-2 focus-within:outline-green-haze-500 '
          type={type} name={name} onChange={onChange} disabled={disabled} placeholder={placeholder} required/>
         </div>
     </div>
