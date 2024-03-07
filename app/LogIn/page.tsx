2c0354141eddf55d7add5f7108c129b335a387ac
unknown <arthur.theodoro.sva@hotmail.com>
2024-03-07 18:14:00 -0300

    created mobile view to login page

--- app/LogIn/page.tsx
+++ app/LogIn/page.tsx
@@ -43,41 +43,48 @@ const LogIn = () => {
     }
   };
   return (
-    <div className="h-screen w-full p-24 flex flex-col items-center justify-center bg-cover bg-woodsmoke-950 bg-green-wallpaper">
-      <div className="h-full w-[50%] glassmorphism flex flex-col items-center p-11 gap-5 ">
-        <div className="h-auto w-full flex flex-row justify-center items-center gap-3">
-          <Image src=" wifi-icon.svg" alt="icon" width={60} height={40} />
-          <h1 className="text-white text-2xl">
-            Tech<span className="text-green-haze-500">Stack</span>
-          </h1>
+    <div className="h-screen w-full py-10 px-4  flex flex-col items-center md:justify-center md:px-10  bg-cover bg-zinc-900 ">
+      <div className="w-full max-h-[60%] md:max-h-[80%] ring-2 h-full flex flex-row px-2 items-center justify-center">
+        <div className="hidden md:block w-1/2 h-full">
+          <Image
+            src="/gray-wallpaper.jpg"
+            width={100}
+            height={100}
+            alt="code banner"
+          />
         </div>
-        <div className="w-full ">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="h-auto items-center flex flex-col gap-3"
-          >
+        <div className="w-full py-4 md:w-1/2 h-full flex flex-col items-center gap-4 border border-green-haze-500 rounded-md shadow-md shadow-green-haze-400">
+          <div className="w-full h-auto px-5 py-2  flex flex-row gap-2 items-center mb-10">
+            <Image
+              src="/wifi-icon.svg"
+              width={30}
+              height={30}
+              alt="wifi icon"
+            />
+            <h1 className="text-white font-bold ">
+              Tech<span className="text-green-haze-500">Stack</span>
+            </h1>
+          </div>
+          <h2 className="text-white text-2xl tracking-widest font-bold">
+            Login
+          </h2>
+          <form className="w-full h-full flex flex-col items-center gap-4">
             <input
               type="text"
-              {...register("email")}
-              className="input w-[80%]"
+              className="bg-transparent h-10 w-[60%] border-b-[1px] border-b-green-haze-500 outline-none text-white font-md px-2 focus:ring-2 focus:ring-green-haze-500 focus:border-none focus:rounded-md"
               placeholder="E-mail"
+              {...register("email")}
             />
             <input
               type="password"
-              {...register("password")}
-              className="input w-[80%]"
+              className="bg-transparent h-10 w-[60%] border-b-[1px] border-b-green-haze-500 outline-none text-white font-md px-2 focus:ring-2 focus:ring-green-haze-500 focus:border-none focus:rounded-md"
               placeholder="Senha"
-              autoComplete="off"
+              {...register("password")}
             />
-            <div className="w-full h-auto flex flex-row justify-center">
-              <Button
-                type="submit"
-                label="Confirmar"
-                variant="primary"
-                classname="w-[60%]"
-                disabled={isSubmitting}
-              />
-            </div>
+
+            <button className="px-10 rounded-md shadow-lg shadow-green-haze-900 py-2 bg-green-haze-700 text-white font-medium hover:bg-green-haze-500 transition-all">
+              Entrar
+            </button>
           </form>
         </div>
       </div>
