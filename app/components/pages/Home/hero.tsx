cbfc9d00e0c3208e93a56bc05a94a957af1f8a0e
unknown <arthur.theodoro.sva@hotmail.com>
2024-07-16 15:39:14 -0300

    feat: home page fully responsive

--- app/components/pages/Home/hero.tsx
+++ app/components/pages/Home/hero.tsx
@@ -5,14 +5,28 @@ import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
-    <motion.div className="h-full overflow-hidden py-10 md:py-0 md:min-h-screen w-full mt-24 md:mt-0 flex flex-col-reverse md:flex-row items-center justify-around gap-10 md:gap-0 bg-zinc-900">
+    <motion.div className="h-full w-full flex flex-col items-center justify-center bg-zinc-900 lg:flex-row-reverse lg:items-center lg:justify-center gap-6 lg:gap-40">
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.4 }}
+        className="flex justify-center w-[250px] galaxy:w-[290px] iphonexr:w-[400px] md:w-[480px] h-auto"
+      >
+        <Image
+          src="/hero-image.svg"
+          alt="banner"
+          sizes="100vw"
+          width={600}
+          height={200}
+        />
+      </motion.div>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
-        className=" flex h-full flex-col md:max-w-[40%] w-auto px-6 iphonexr:px-5 gap-2 items-center justify-center"
+        className="flex h-auto flex-col w-full px-6 iphonexr:px-5 gap-4 md:px-10 items-center justify-center lg:w-[50%]"
       >
-        <h1 className=" text-2xl iphonexr:text-4xl md:text-5xl text-white font-bold mb-8">
+        <h1 className=" text-2xl iphonexr:text-4xl md:text-5xl text-white font-bold mb-4 lg:text-6xl">
           Crie <span className="text-green-haze-500">publicações</span> sobre os
           mais variados campos da{" "}
           <span className="text-green-haze-500">tecnologia</span>, veja posts e
@@ -22,7 +36,7 @@ const HeroSection = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.2, delay: 0.2 }}
-          className="text-green-haze-500 px-2 text-sm md:text-md text-justify whitespace-pre-line"
+          className="text-green-haze-500 px-2 text-sm md:text-lg text-justify whitespace-pre-line"
         >
           Publique artigos, dicas ou expêriencias que você deseja compartilhar,
           aqui você poderá encontrar pessoas que estão trilhando o caminho para
@@ -51,20 +65,6 @@ const HeroSection = () => {
         </motion.div>
       </motion.div>
       {/* mobile image */}
-      <motion.div
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.4 }}
-        className="w-[250px] galaxy:w-[290px] iphonexr:w-[400px] h-auto"
-      >
-        <Image
-          src="/hero-image.svg"
-          alt="banner"
-          sizes="100vw"
-          width={400}
-          height={200}
-        />
-      </motion.div>
     </motion.div>
   );
 };
